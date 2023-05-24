376fb8cb8efcbfc635a681a674d6c41db3422707
caiomarqs <caiomarques1412@gmail.com>
2023-05-24 15:36:45 -0300

    Change action

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const core = require('@actions/core');
 const wait = require('./wait');
+const fs = require('fs');
 
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    // const ms = core.getInput('milliseconds');
+    // core.info(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // await wait(parseInt(ms));
+    // core.info((new Date()).toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    // core.setOutput('time', new Date().toTimeString());
+
+
+    var path = process.argv[2];
+
+    fs.readdir(path, function (err, items) {
+      console.log(items);
+
+      for (var i = 0; i < items.length; i++) {
+        console.log(items[i]);
+      }
+    })
   } catch (error) {
     core.setFailed(error.message);
   }
