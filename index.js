a862f5c88b26208c0cbd0ebbeeca930fde76a7c6
caiomarqs <caiomarques1412@gmail.com>
2023-06-12 01:00:18 -0300

    Fix for subfolder on git repository

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const core = require('@actions/core')
 const fsAsync = require('fs').promises
 const fs = require('fs')
+const path = require('path')
 const gdrive = require('./lib/gdrive')
 require('dotenv').config()
 
 const run = async () => {
+
+
   try {
     const folderPath = core.getInput('folderToUpload')
     const ignorePaths = core.getInput('ignorePaths').split(',')
     const driveFolderId = core.getInput('driveFolderId')
-
+    
     await gdrive.deleteFilesFolder(driveFolderId)
 
-    fs.readdir(folderPath, (err, items) => {
-      
-      core.info('Upload files to drive')
+    const completeFolderPath = path.join(__dirname, folderPath)
 
-      core.info(items)
+    fs.readdir(completeFolderPath, (err, items) => {
+      core.info('Upload files to drive')
 
       items.forEach(
         async item => await gdrive.uploadFile(
+          completeFolderPath,
           item,
           driveFolderId,
           ignorePaths
